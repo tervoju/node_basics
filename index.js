793e424026165ddfb99b2c6443c133c0efe29094
juha tervo <jutervo@outlook.com>
2018-06-26 13:52:14 +0300

    config file

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require('express')
+const config = require('./config')
 //const compression = require('compression')
 //const debug = require('debug')
 const hbs = require('hbs')
 const app = express()
-const PORT = 3000
+const PORT = config.port
 
 //debug('booting')
 //app.use(compression())
 hbs.registerPartials(__dirname + '/views/partials')
-hbs.registerHelper
 app.set('view engine', 'hbs')
 
 app.use(express.static(__dirname + '/public'))
 app.use((req,res, next) => {
-    res.render('maintenance.hbs, {
+    res.render('maintenance.hbs', {
         title: 'HG110',
         text: 'hiljainen huone',
         cups: '-'
@@ -30,6 +30,5 @@ app.get('/', (req, res) => {
 
 console.log (process.env)
 app.listen(PORT, () => {
-    console.log("listening")
-    //debug('listening port:'+ PORT)
+    console.log('listening: '+ PORT)
 })
